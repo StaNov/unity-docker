3f5983d647fac4c8eef63b0cfd26ca157d9086b5
StaNov <info@stanov.cz>
2020-10-31 14:11:25 +0100

    selenium activation stub

--- activation/activate.py
+++ activation/activate.py
@@ -1,24 +1,21 @@
 import os
 from time import sleep
 
-from selenium.webdriver import FirefoxProfile
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox import webdriver
 from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.expected_conditions import presence_of_element_located, element_to_be_clickable
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.wait import WebDriverWait
 
 email = os.environ['UNITY_EMAIL']
 password = os.environ['UNITY_PASSWORD']
 
-profile = FirefoxProfile()
+options = ChromeOptions()
 
-profile.set_preference("browser.download.folderList", 2)
-profile.set_preference("browser.download.manager.showWhenStarting", False)
-profile.set_preference("browser.download.dir", os.path.abspath("."))
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/plain, application/octet-stream, application/binary, text/csv, application/csv, application/excel, text/comma-separated-values, text/xml, application/xml")
-
-with webdriver.WebDriver(profile) as ff:
+# with WebDriver(options=options) as ff:
+with webdriver.Remote(command_executor="http://localhost:4444/wd/hub", options=options) as ff:
     ff.get("https://id.unity.com")
     emailField = ff.find_element_by_id("conversations_create_session_form_email")
     emailField.send_keys(email)
@@ -27,8 +24,7 @@ with webdriver.WebDriver(profile) as ff:
     ff.find_element_by_css_selector("input[type=submit]").click()
 
     ff.get("https://license.unity3d.com/manual")
-    WebDriverWait(ff, 5).until(expected_conditions.url_contains("oauth"))
-    WebDriverWait(ff, 5).until(expected_conditions.url_to_be("https://license.unity3d.com/manual"))
+    sleep(5)  # reload the manual page after credentials check, chrome doesn't allow to check the URL
 
     licenseFilePath = os.path.abspath("UnityRequestFile.alf")
     print("License file path: " + licenseFilePath)
@@ -39,8 +35,8 @@ with webdriver.WebDriver(profile) as ff:
 
     WebDriverWait(ff, 5).until(presence_of_element_located((By.CSS_SELECTOR, "label[for=type_personal]"))).click()
     ff.find_element_by_css_selector("label[for=option3]").click()
-    ff.find_element_by_css_selector(".option-personal input[type=submit]").click()
+    WebDriverWait(ff, 5).until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, ".option-personal input[type=submit]"))).click()
 
-    ff.find_element_by_css_selector("input[type=submit]").click()
+    WebDriverWait(ff, 5).until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, "input[type=submit]"))).click()
 
-    sleep(5)
+    sleep(5)  # let the download finish
